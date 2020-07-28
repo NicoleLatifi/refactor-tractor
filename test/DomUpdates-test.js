517205443651489d2fb152d786047a0ea5936ca0
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-27 19:47:07 -0600

    Simplified test setup and updated test name based off of group mates suggestions #59

--- test/DomUpdates-test.js
+++ test/DomUpdates-test.js
@@ -1,10 +1,7 @@
-// My package-lock.json changed. I think when I ran npm install chai-spies
 import DomUpdates from '../src/DomUpdates';
-import User from '../src/User'
 const chai = require('chai');
 const spies = require('chai-spies');
 const expect = chai.expect;
-
 
 chai.use(spies);
 
@@ -12,26 +9,14 @@ describe.only('DomUpdates', function() {
   let domUpdates;
   let user;
   beforeEach(() => {
-    domUpdates = new DomUpdates(); // do we need to include parameters here?
-    user = new User({
-      'id': 1,
-      'name': 'Luisa Hane',
-      'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
-      'email': 'Diana.Hayes1@hotmail.com',
-      'strideLength': 4.3,
-      'dailyStepGoal': 10000,
-      'friends': [
-        16,
-        4,
-        8
-      ]
-    });
+    domUpdates = new DomUpdates();
+    user = {};
   });
   afterEach(function() {
     chai.spy.restore(domUpdates)
   });
 
-  it('Should spy on updateDropdown', function() {
+  it('Should be able to update the dropdown menu with user information', function() {
     global.domUpdates
     chai.spy.on(domUpdates, ['updateDropdown'], () => {});
 
