c6064b54ab5d9b04298f26580584e2cab84d1b28
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-27 19:09:37 -0600

    Add afterEach to DomUpdates-test #60

--- test/DomUpdates-test.js
+++ test/DomUpdates-test.js
@@ -1,7 +1,43 @@
+// My package-lock.json changed. I think when I ran npm install chai-spies
+import DomUpdates from '../src/DomUpdates';
+import User from '../src/User'
+const chai = require('chai');
 const spies = require('chai-spies');
-const { expect } = require('chai');
+const expect = chai.expect;
+
 
 chai.use(spies);
 
-global.domUpdates
-chai.spy.on(domUpdates, ['updateDropdown', () => {}]);
+describe.only('DomUpdates', function() {
+  let domUpdates;
+  let user;
+  beforeEach(() => {
+    domUpdates = new DomUpdates(); // do we need to include parameters here?
+    user = new User({
+      'id': 1,
+      'name': 'Luisa Hane',
+      'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
+      'email': 'Diana.Hayes1@hotmail.com',
+      'strideLength': 4.3,
+      'dailyStepGoal': 10000,
+      'friends': [
+        16,
+        4,
+        8
+      ]
+    });
+  });
+  afterEach(function() {
+    chai.spy.restore(domUpdates)
+  });
+
+  it('Should spy on updateDropdown', function() {
+    global.domUpdates
+    chai.spy.on(domUpdates, ['updateDropdown'], () => {});
+
+    domUpdates.updateDropdown(user)
+
+    expect(domUpdates.updateDropdown).to.have.been.called(1);
+    expect(domUpdates.updateDropdown).to.have.been.called.with(user);
+  })
+})
